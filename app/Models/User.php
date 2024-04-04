cfdbd1feb15171473cb0787607191f533c46363c
Paul Garcia <paulgr30@hotmail.com>
2024-04-03 22:54:36 -0500

    change:configuramos el modelo user para la autenticacion y los roles y permisos

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,10 +9,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
-    use HasApiTokens, HasFactory, Notifiable, BaseScope, UserScope;
+    use HasApiTokens, HasFactory, Notifiable, BaseScope, UserScope, HasRoles;
 
     /**
      * The attributes that are mass assignable.
@@ -55,5 +57,33 @@ class User extends Authenticatable
     public function orders()
     {
         return $this->hasMany(Order::class);
+    }
+
+
+
+
+
+
+
+
+
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
