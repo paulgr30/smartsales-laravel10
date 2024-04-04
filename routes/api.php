52a8977ba1ad2ca0a6b5388e2f42f0af7ad7af0f
Paul Garcia <paulgr30@hotmail.com>
2024-04-04 13:17:03 -0500

    feat:agregar las rutas para la autenticacion y middleware

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,13 @@
 <?php
 
 use App\Http\Controllers\{
+    AuthController,
     CategoryController,
     CustomerController,
     IdentityDocumentController,
     OrderController,
     OrderStatusController,
     ProductController,
-    QuoteController,
     UnitController
 };;
 
@@ -25,65 +25,118 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+/*Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});
+});*/
+
+
+
+
+// Autenticacion
+Route::post('auth/login', [AuthController::class, 'login'])->name('auth.login');
+
+//middleware('jwt.auth')->
+Route::middleware('jwt.auth')->controller(AuthController::class)->prefix('auth')
+    ->group(
+        function () {
+            Route::get('profile', 'getProfile')->name('auth.profile');
+            Route::put('profile/update', 'updateProfile')->name('auth.profile.update');
+            Route::post('password/change', 'changePassword')->name('auth.password.change');
+            Route::post('token/refresh', 'refreshToken')->name('auth.token.refresh');
+            Route::post('logout', 'logout')->name('auth.logout');
+        }
+    );
 
 
 
 
 // Categories
-Route::get('categories', [CategoryController::class, 'getAll']);
-Route::get('categories/bycriteria', [CategoryController::class, 'getByCriteria']);
-Route::post('categories', [CategoryController::class, 'store']);
-Route::put('categories/{category}', [CategoryController::class, 'update']);
-Route::patch('categories/{category}', [CategoryController::class, 'toggleStatus']);
-Route::delete('categories/{category}', [CategoryController::class, 'destroy']);
+Route::middleware('jwt.auth')->controller(CategoryController::class)
+    ->group(
+        function () {
+            Route::get('categories', 'getAll')->name('categories.all');
+            Route::get('categories/bycriteria', 'getByCriteria')->name('categories.bycriteria');
+            Route::post('categories', 'store')->name('categories.store');
+            Route::put('categories/{category}', 'update')->name('categories.update');
+            Route::patch('categories/{category}', 'toggleStatus')->name('categories.toggle.status');
+            Route::delete('categories/{category}', 'destroy')->name('categories.destroy');
+        }
+    );
 
 
 // Units
-Route::get('units', [UnitController::class, 'getAll']);
-Route::get('units/bycriteria', [UnitController::class, 'getByCriteria']);
-Route::post('units', [UnitController::class, 'store']);
-Route::put('units/{unit}', [UnitController::class, 'update']);
-Route::patch('units/{unit}', [UnitController::class, 'toggleStatus']);
-Route::delete('units/{unit}', [UnitController::class, 'destroy']);
+Route::middleware('jwt.auth')->controller(UnitController::class)
+    ->group(
+        function () {
+            Route::get('units', 'getAll');
+            Route::get('units/bycriteria', 'getByCriteria');
+            Route::post('units', 'store');
+            Route::put('units/{unit}', 'update');
+            Route::patch('units/{unit}', 'toggleStatus');
+            Route::delete('units/{unit}', 'destroy');
+        }
+    );
 
 
 // IdentityDocument
-Route::get('identitydocuments', [IdentityDocumentController::class, 'getAll']);
-Route::get('identitydocuments/bycriteria', [IdentityDocumentController::class, 'getByCriteria']);
-Route::post('identitydocuments', [IdentityDocumentController::class, 'store']);
-Route::put('identitydocuments/{identityDocument}', [IdentityDocumentController::class, 'update']);
-Route::patch('identitydocuments/{identityDocument}', [IdentityDocumentController::class, 'toggleStatus']);
-Route::delete('identitydocuments/{identityDocument}', [IdentityDocumentController::class, 'destroy']);
+Route::middleware('jwt.auth')->controller(IdentityDocumentController::class)
+    ->group(
+        function () {
+            Route::get('identitydocuments', 'getAll');
+            Route::get('identitydocuments/bycriteria', 'getByCriteria');
+            Route::post('identitydocuments', 'store');
+            Route::put('identitydocuments/{identityDocument}', 'update');
+            Route::patch('identitydocuments/{identityDocument}', 'toggleStatus');
+            Route::delete('identitydocuments/{identityDocument}', 'destroy');
+        }
+    );
 
 
 // Product
-Route::get('products', [ProductController::class, 'getAll']);
-Route::get('products/bycriteria', [ProductController::class, 'getByCriteria']);
-Route::post('products', [ProductController::class, 'store']);
-Route::put('products/{product}', [ProductController::class, 'update']);
-Route::patch('products/{product}', [ProductController::class, 'toggleStatus']);
-Route::delete('products/{product}', [ProductController::class, 'destroy']);
+Route::middleware('jwt.auth')->controller(IdentityDocumentController::class)
+    ->group(
+        function () {
+            Route::get('products', [ProductController::class, 'getAll']);
+            Route::get('products/bycriteria', [ProductController::class, 'getByCriteria']);
+            Route::post('products', [ProductController::class, 'store']);
+            Route::put('products/{product}', [ProductController::class, 'update']);
+            Route::patch('products/{product}', [ProductController::class, 'toggleStatus']);
+            Route::delete('products/{product}', [ProductController::class, 'destroy']);
+        }
+    );
 
 
 // Users
-Route::get('users', [CustomerController::class, 'getAll']);
-Route::get('users/bycriteria', [CustomerController::class, 'getByCriteria']);
-Route::get('users/bynumberid', [CustomerController::class, 'getByNumberId']);
-Route::post('users', [CustomerController::class, 'store']);
-Route::put('users/{user}', [CustomerController::class, 'update']);
-Route::delete('users/{user}', [CustomerController::class, 'destroy']);
+Route::middleware('jwt.auth')->controller(CustomerController::class)
+    ->group(
+        function () {
+            Route::get('users', 'getAll');
+            Route::get('users/bycriteria', 'getByCriteria');
+            Route::get('users/bynumberid', 'getByNumberId');
+            Route::post('users', 'store');
+            Route::put('users/{user}', 'update');
+            Route::delete('users/{user}', 'destroy');
+        }
+    );
 
 
 // Statuses
-Route::get('statuses', [OrderStatusController::class, 'getAll']);
+Route::middleware('jwt.auth')->controller(OrderStatusController::class)
+    ->group(
+        function () {
+            Route::get('statuses', 'getAll');
+        }
+    );
 
 
 // Orders
-Route::get('orders', [OrderController::class, 'getAll']);
-Route::get('orders/bycriteria', [OrderController::class, 'getByCriteria']);
-Route::post('orders', [OrderController::class, 'store']);
-Route::put('orders/{order}', [OrderController::class, 'update']);
-Route::patch('orders/{order}', [OrderController::class, 'canceled']);
+Route::middleware('jwt.auth')->controller(OrderController::class)
+    ->group(
+        function () {
+            Route::get('orders', 'getAll');
+            Route::get('orders/bycriteria', 'getByCriteria');
+            Route::post('orders', 'store');
+            Route::put('orders/{order}', 'update');
+            Route::patch('orders/{order}', 'canceled');
+        }
+    );
