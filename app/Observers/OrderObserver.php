0098fc85e3ba1febeaa0c386ca7bb46c1a265885
Paul Garcia <paulgr30@hotmail.com>
2024-04-17 13:24:43 -0500

    change:se cambio el usuario fijo por el usuario autenticado

--- app/Observers/OrderObserver.php
+++ app/Observers/OrderObserver.php
@@ -3,12 +3,13 @@
 namespace App\Observers;
 
 use App\Models\Order;
+use Illuminate\Support\Facades\Auth;
 
 class OrderObserver
 {
     public function creating(Order $order): void
     {
-        $order->user_id = 1;
+        $order->user_id = Auth::user()->id;
         $order->order_status_id = 1;
 
         $lastOrderNumber = Order::max('number');
